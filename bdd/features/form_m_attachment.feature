bc3e9b3718aedaedf0fbc71acac3f65eca766c57
maneptha <maneptha@gmail.com>
2016-04-04 07:58:32 +0100

    attachment: wip

--- bdd/features/form_m_attachment.feature
+++ bdd/features/form_m_attachment.feature
@@ -1,5 +1,6 @@
 @form_m_detail_interface
 @form_m_attachment_interface
+@co
 Feature: Form M Attachment
   As a user
   I want to be able to add attachments to form M
@@ -16,9 +17,14 @@ Feature: Form M Attachment
     And form M already saved in the system
     Given There is data about attachment to be saved
     When I fetch the saved form M
-    Then I notice a plus-sign icon preceding text 'Add attachment'
+    Then I notice a minus-sign icon preceding text 'Add attachment'
     And I notice 'Add attachment' form is visible
     And I notice 'Add attachment' button is disabled
+    And I notice there is no indication that any file has been selected
     When I fill attachment title field
     And I choose attachment file
+    Then I notice selected file name has appeared in the form
     Then I notice 'Add attachment' button is enabled
+    When I click 'Add attachment' button
+    Then I notice 'Add attachment' form is not visible
+
--- bdd/features/steps/form_m_attachment.py
+++ bdd/features/steps/form_m_attachment.py
@@ -1,12 +1,15 @@
 from behave import *
 import nose.tools as nt
 import os
+import time
 
 use_step_matcher("re")
 
+add_attachment_form_btn_id = 'add-attachment-form-submit-btn'
+
 
 def add_attachment_button_is_disabled(context):
-    return context.browser.driver.find_element_by_id('add-attachment-form-submit-btn').get_attribute('disabled')
+    return context.browser.driver.find_element_by_id(add_attachment_form_btn_id).get_attribute('disabled')
 
 
 @given("There is data about attachment to be saved")
@@ -34,14 +37,16 @@ def step_impl(context):
     """)
 
 
-@then("I notice a plus-sign icon preceding text 'Add attachment'")
+@then("I notice a minus-sign icon preceding text 'Add attachment'")
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
+    # time.sleep(10)
+    context.attachment_minus_toggle_selector = '.add-attachment-form-toggle>.glyphicon-minus-sign'
     nt.assert_true(
-            context.browser.is_element_present_by_css('.add-attachment-form-toggle>.glyphicon-minus-sign', wait_time=1),
-            'add attachment form toggle plus icon must be visible')
+            context.browser.is_element_present_by_css(context.attachment_minus_toggle_selector, wait_time=1),
+            'Add attachment form toggle minus icon must be visible')
 
 
 @step("I notice 'Add attachment' form is visible")
@@ -49,8 +54,9 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
+    context.add_attachment_form_name = 'addAttachmentForm'
     nt.assert_true(
-            context.browser.is_element_present_by_name('addAttachmentForm'),
+            context.browser.is_element_present_by_name(context.add_attachment_form_name),
             'Add attachment form must be visible'
     )
 
@@ -60,7 +66,25 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    nt.assert_true(add_attachment_button_is_disabled(context), 'Add attachment form submit button must be disabled')
+    nt.assert_true(
+            add_attachment_button_is_disabled(context),
+            'Add attachment form submit button must be disabled'
+    )
+
+
+@step("I notice there is no indication that any file has been selected")
+def step_impl(context):
+    """
+    When a file has been selected, div.form-m-selected-attachment-file-display will appear in the DOM for
+    every file selected
+
+    :type context: behave.runner.Context
+    """
+    context.attachment_selected_display_selector = '.form-m-selected-attachment-file-display'
+    nt.assert_false(
+            context.browser.is_element_present_by_css(context.attachment_selected_display_selector),
+            'There must be no indication that a file has been selected'
+    )
 
 
 @when("I fill attachment title field")
@@ -68,7 +92,8 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    context.browser.fill('attachmentTitle', context.attachment_data['title'])
+    context.add_attachment_form_title_field_name = 'attachmentTitle'
+    context.browser.fill(context.add_attachment_form_title_field_name, context.attachment_data['title'])
 
 
 @step("I choose attachment file")
@@ -76,7 +101,20 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    pass
+    # files_field = context.browser.driver.find_element_by_id('add-attachment-files-select')
+    # files_field.send_keys(os.path.join(os.path.abspath(__file__), 'attachment-1.txt'))
+    context.browser.attach_file('files', os.path.join(os.path.abspath(__file__), 'attachment-1.txt'))
+
+
+@then("I notice selected file name has appeared in the form")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    # time.sleep(5)
+    selected_file_display = context.browser.find_by_css(context.attachment_selected_display_selector)
+    nt.eq_(len(selected_file_display), 1, 'There must be one selected file displayed')
+    nt.assert_in('1. attachment-1.txt', selected_file_display.first.text)
 
 
 @then("I notice 'Add attachment' button is enabled")
@@ -84,4 +122,28 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    pass
+
+    # time.sleep(10)
+    nt.assert_false(add_attachment_button_is_disabled(context), 'Add attachment form submit button must be enabled')
+
+
+@when("I click 'Add attachment' button")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    add_attachment_form_btn = context.browser.find_by_id(add_attachment_form_btn_id).first
+    print('\n\n\n\n\n\nadd_attachment_form_btn = ', add_attachment_form_btn, '\n\n\n\n\n\n\n\n\n')
+    add_attachment_form_btn.click()
+    time.sleep(20)
+
+
+@then("I notice 'Add attachment' form is not visible")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    nt.assert_false(
+            context.browser.is_element_present_by_name(context.add_attachment_form_name, wait_time=1),
+            'Add attachment form must not be visible'
+    )
