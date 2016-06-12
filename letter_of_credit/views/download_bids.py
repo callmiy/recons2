348435f33d7d66f55314002ccccb86b6e9ae666f
maneptha <maneptha@gmail.com>
2016-06-12 19:10:18 +0100

    download bid report - columns re-arranged according to new treasury format

--- letter_of_credit/views/download_bids.py
+++ letter_of_credit/views/download_bids.py
@@ -1,16 +1,84 @@
+from datetime import datetime, date
+
 from django.http import HttpResponse
 from django.views.generic import View
-from letter_of_credit.models import LcBidRequest
 from openpyxl import Workbook
+from openpyxl.styles import Font, Alignment
 from openpyxl.writer.excel import save_virtual_workbook
-from openpyxl.styles import Font
-from datetime import datetime
+
+from letter_of_credit.models import LcBidRequest
 
 
 class DownloadBidsView(View):
+    def set_header_row(self, sheet, mark_as_downloaded):
+        font = Font(bold=True, name='Rockwell')
+        alignment = Alignment(wrap_text=True, horizontal='center', vertical='center')
+
+        header_col_1 = sheet.cell(row=1, column=1, value='DATE')
+        header_col_1.font = font
+        header_col_1.alignment = alignment
+
+        s = sheet.cell(row=1, column=2, value='FORM M NUMBER')
+        s.font = font
+        s.alignment = alignment
+
+        s = sheet.cell(row=1, column=3, value='LC/BC REF')
+        s.font = font
+        s.alignment = alignment
+
+        s = sheet.cell(row=1, column=4, value='CUSTOMER NAME')
+        s.font = font
+        s.alignment = alignment
+
+        s = sheet.cell(row=1, column=5, value='CURRENCY')
+        s.font = font
+        s.alignment = alignment
+
+        s = sheet.cell(row=1, column=6, value='ACTUAL BID AMOUNT')
+        s.font = font
+        s.alignment = alignment
+
+        s = sheet.cell(row=1, column=7, value='ALLOCATION')
+        s.font = font
+        s.alignment = alignment
+
+        s = sheet.cell(row=1, column=8, value='OUTSTANDING BALANCE')
+        s.font = font
+        s.alignment = alignment
+
+        s = sheet.cell(row=1, column=9, value='ALLOCATION')
+        s.font = font
+        s.alignment = alignment
+
+        s = sheet.cell(row=1, column=10, value='ACCOUNT NOS')
+        s.font = font
+        s.alignment = alignment
+
+        s = sheet.cell(row=1, column=11, value='ITEM OF IMPORT')
+        s.font = font
+        s.alignment = alignment
+
+        s = sheet.cell(row=1, column=12, value='MATURITY DATE')
+        s.font = font
+        s.alignment = alignment
+
+        s = sheet.cell(row=1, column=13, value='CATEGORY')
+        s.font = font
+        s.alignment = alignment
+
+        if not mark_as_downloaded:
+            # we are downloading all bids
+            header_col_1.value = 'S/N'
+            s = sheet.cell(row=1, column=14, value='DATE SENT TO TREASURY')
+            s.font = font
+            s.alignment = alignment
+
+            s = sheet.cell(row=1, column=15, value='REMARK')
+            s.font = font
+            s.alignment = alignment
+
     def get(self, request):
         wb = Workbook()
-        sheet = wb.active
         bid_ids = request.GET.getlist('bid_ids')
         mark_as_downloaded = False
 
@@ -22,37 +90,33 @@ class DownloadBidsView(View):
             bid_ids = LcBidRequest.objects.values_list('pk', flat=True)
 
         if bid_ids:
-            font = Font(bold=True)
-            sheet.cell(row=1, column=1, value='S/N').font = font
-            sheet.cell(row=1, column=2, value='CUSTOMER').font = font
-            sheet.cell(row=1, column=3, value='CURR').font = font
-            sheet.cell(row=1, column=4, value='AMOUNT').font = font
-            sheet.cell(row=1, column=5, value='PURPOSE').font = font
-            sheet.cell(row=1, column=6, value='A/C NO.').font = font
-            sheet.cell(row=1, column=7, value='MF NO').font = font
-            sheet.cell(row=1, column=8, value='LC REF.').font = font
-            sheet.cell(row=1, column=9, value='MATURITY DATE').font = font
-            sheet.cell(row=1, column=10, value='ESTB. DATE').font = font
-
-            if not mark_as_downloaded:
-                # we are downloading all bids
-                sheet.cell(row=1, column=11, value='TOTAL ALLOCATION').font = font
-                sheet.cell(row=1, column=12, value='UNALLOCATED').font = font
-                sheet.cell(row=1, column=13, value='DATE SENT TO TREASURY').font = font
-                sheet.cell(row=1, column=14, value='REMARK').font = font
-
+            sheet = wb.active
+            self.set_header_row(sheet, mark_as_downloaded)
             row = 2
             row_index = 1
 
             for bid in LcBidRequest.objects.filter(pk__in=bid_ids):
                 mf = bid.mf
                 applicant = mf.applicant
+                sheet.cell(
+                        row=row, column=1, value=mark_as_downloaded and date.today().strftime('%d-%b-%Y') or row_index)
+                sheet.cell(row=row, column=2, value=mf.number)
 
-                sheet.cell(row=row, column=1, value=row_index)
-                sheet.cell(row=row, column=2, value=applicant.name)
-                sheet.cell(row=row, column=3, value=mf.currency.code)
-                sheet.cell(row=row, column=4, value=bid.amount)
-                sheet.cell(row=row, column=5, value=mf.goods_description)
+                lc_number = 'NEW LC'
+                lc = mf.lc
+
+                if lc:
+                    lc_number = lc.lc_number
+
+                sheet.cell(row=row, column=3, value=lc_number)
+                sheet.cell(row=row, column=4, value=applicant.name)
+                sheet.cell(row=row, column=5, value=mf.currency.code)
+
+                total_allocation = sum([allocation['amount_allocated'] for allocation in bid.allocations()])
+                outstanding_bid_amount = bid.amount - total_allocation
+                sheet.cell(row=row, column=6, value=outstanding_bid_amount)
+                sheet.cell(row=row, column=7, value=total_allocation)
+                sheet.cell(row=row, column=8, value=(outstanding_bid_amount))
 
                 acct = ''
                 acct_numbers_qs = applicant.acct_numbers
@@ -60,39 +124,19 @@ class DownloadBidsView(View):
                 if acct_numbers_qs:
                     acct = acct_numbers_qs[0].nuban
 
-                sheet.cell(row=row, column=6, value=acct)
-                lc_number = 'NEW LC'
-                estb_date = ''
-                lc = mf.lc
-
-                if lc:
-                    lc_number = lc.lc_number
-                    estb_date = lc.estb_date.strftime('%Y-%m-%d')
-
-                sheet.cell(row=row, column=7, value=mf.number)
-                sheet.cell(row=row, column=8, value=lc_number)
-                sheet.cell(row=row, column=10, value=estb_date)
-                maturity = 'CASH BACKED'
-
-                if bid.maturity:
-                    maturity = mark_as_downloaded and bid.maturity.strftime('%d-%b-%Y') or bid.maturity
-
-                sheet.cell(row=row, column=9, value=maturity)
-                remark = ''
-
-                if bid.deleted_at:
-                    remark = 'bid deleted'
-                elif mf.deleted_at:
-                    remark = 'form M cancelled'
-
-                sheet.cell(row=row, column=11, value=remark)
+                sheet.cell(row=row, column=10, value=acct)
+                sheet.cell(row=row, column=11, value=mf.goods_description)
 
                 if not mark_as_downloaded:
-                    total_allocation = sum([allocation['amount_allocated'] for allocation in bid.allocations()])
-                    sheet.cell(row=row, column=11, value=total_allocation)
-                    sheet.cell(row=row, column=12, value=(bid.amount - total_allocation))
-                    sheet.cell(row=row, column=13, value=bid.requested_at)
-                    sheet.cell(row=row, column=14, value=remark)
+                    remark = ''
+
+                    if bid.deleted_at:
+                        remark = 'bid deleted'
+                    elif mf.deleted_at:
+                        remark = 'form M cancelled'
+
+                    sheet.cell(row=row, column=14, value=bid.requested_at)
+                    sheet.cell(row=row, column=15, value=remark)
 
                 row += 1
                 row_index += 1
@@ -100,6 +144,9 @@ class DownloadBidsView(View):
                 if mark_as_downloaded and not bid.downloaded:
                     bid.downloaded = True
                     bid.save()
+            if not mark_as_downloaded:
+                # hide the column with heading 'allocation' as this is only necessary when sending to treasury
+                column_dimension = sheet.column_dimensions['I'].hidden = True
 
         resp = HttpResponse(save_virtual_workbook(wb), content_type='application/vnd.ms-excel')
         resp['Content-Disposition'] = 'attachment; filename="%s"' % file_name
